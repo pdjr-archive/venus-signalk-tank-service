ec0b57eb1341ab77869fa7d7d3fbce5750520173
Paul Reeve <preeve@pdjr.eu>
2021-02-10 09:41:00 +0000

    First upload

--- README.md
+++ README.md
@@ -1,1 +1,111 @@
 # venus-signalk-tank-service
+
+__venus-signalk-tank-service__ implements a solution for displaying
+tank data derived from multi-channel tank sensors like the Maretron
+FPM100 and the Garnet SeeLevel on a Venus OS device like the Victron
+CCGX.
+
+## Background
+
+Support for tank monitoring in Venus OS is fundamentally broken.
+Inexplicably, Victron's low-level code ignores CAN/N2K tank instance
+numbers, substituting them with the sensor device's own instance
+number.
+Since the Victron strategy is to associate each tank sensor with a
+dbus service, the consequence is that data from multiple tanks appears
+in Venus as a single dbus service.
+In the absence of any tank instance numbers, disaggregation of the
+composite data is at best problematic and for non-trivial tank
+installations infeasible.
+
+Even so, there have been a number of attempts at implementing fixes
+and work-arounds in Venus, mostly relying on the spurious association
+of fluid type and tank level which allows the disaggregation of the
+composite data if one allows no more than one tank of any fluid type.
+kwindrem's
+("repeater")[https://github.com/kwindrem/SeeLevel-N2K-Victron-VenusOS]
+project follows this approach by:
+
+1. disaggregating the garbled data and creating one dbus service for
+   each identified tank, and
+2. tweaking the Venus GUI so that data from the new dbus services is
+   displayed nicely and data from the garbled, original, service is
+   ignored.
+
+## This project
+
+__venus-signalk-tank-service__ borrows kwindrem's GUI modifications,
+ignores Venus' broken tank handling and instead recovers tank data
+from a Signal K server, generates one dbus service per tank and in
+so doing makes it available to Venus in a way that can be picked up
+and rendered on a CCGX or similar display.
+
+Data is recovered from Signal K over HTTP and the Signal K server can
+be running on the local network or even on the Venus host.
+
+Pretty rendering of the tank data on a CCGX or whatever depends upon
+kwindrem's GUI tweaks.
+
+### Installation
+
+1. If you are installing on a CCGX, make sure that root access is
+   enabled.
+   
+2. Install kwindrem's repeater project by following his instructions
+   at the above link.
+   
+   When you run the repeater project setup script, respond to the
+   first prompts with 'a' (Activate) and subsequent prompts with 'y'.
+   This will activate kwindrem's repeater (we don't need this) and
+   install his GUI changes (we do need these).
+   
+   Run the repeater project setup script again, responding to the
+   first prompt with 'd' (Disable) and subsequent prompts with 'y'.
+   This will disable kwindrem's repeater, but leave his GUI changes
+   in place.
+   
+3. Install __venus-signalk-tank-service__ by logging into your Venus
+   device and issuing the following commands.
+```
+$> cd /data
+$> wget wget https://github.com/preeve9534/venus-signalk-tank-service/archive/main.zip
+$> unzip main.zip
+$> cd venus-signalk-tank-service
+```
+
+4. Open signalktankservice.py in a text editor and change the value
+   of SIGNALK_SERVER and SIGNALK_TANKS to suit your needs.
+
+5. Run signalktankservice.py and check that it outputs details of the
+   tanks it is configuring. My system has five tanks and I see:
+   ```
+   $> ./signalktankservice.py 
+   INFO:root:registered ourselves on D-Bus as com.victronenergy.tank.signalk_tank_0
+   INFO:root:registered ourselves on D-Bus as com.victronenergy.tank.signalk_tank_1
+   INFO:root:registered ourselves on D-Bus as com.victronenergy.tank.signalk_tank_2
+   INFO:root:registered ourselves on D-Bus as com.victronenergy.tank.signalk_tank_3
+   INFO:root:registered ourselves on D-Bus as com.victronenergy.tank.signalk_tank_4
+   ```
+   If the output isn't what you expect, then check the tank data
+   available in Signal K and make sure that the values you supplied
+   for SIGNALK_SERVER and SIGNALK_TANKS are correct.
+
+6. With ```signalktankservice.py``` running you should see your
+   configured tanks displaying on the Venus GUI.
+
+7. Run ```setup``` to make ```signalktankservice.py``` execute
+   automatically when Venus boots.
+
+
+
+to the
+   two questions relating to GUI changes.
+
+2. 
+
+#
+# The code here uses the GUI enhancements implemented by kwindrem in
+# the project mentioned above.
+#
+# 1. Install the GUI enhancements from kwindrem's project.
+# 2. Copy this program into  
---
+++ signalktankservice.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+
+########################################################################
+# Copyright 2021 Paul Reeve <preeve@pdjr.eu>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License. You may
+# obtain a copy of the License at
+#
+#	 http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import gobject
+import platform
+import argparse
+import logging
+import sys
+import os
+import dbus
+import httplib
+import json
+
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), './ext/velib_python'))
+from vedbus import VeDbusService
+
+########################################################################
+# START OF USER CONFIGURATION
+########################################################################
+
+# Change this value to specify your Signal K server.
+
+SIGNALK_SERVER = '192.168.1.2:3000'
+
+# Specify an empty array and all tank paths on the Signal K server
+# will be automatically acquired. Otherwise, specify the tanks you
+# want to handle like this.
+
+SIGNALK_TANKS = [
+	{ 'path': 'tanks/wasteWater/0' },
+	{ 'path': 'tanks/freshWater/1' },
+	{ 'path': 'tanks/freshWater/2' },
+	{ 'path': 'tanks/fuel/3' },
+	{ 'path': 'tanks/fuel/4' }
+]
+
+# The frequency in ms at which to update tank data.
+
+UPDATE_INTERVAL = 10000
+
+########################################################################
+# END OF USER CONFIGURATION
+########################################################################
+
+SIGNALK_SELF_PATH='/signalk/v1/api/vessels/self'
+SIGNALK_TO_N2K_FLUID_TYPES = { 'fuel': 0, 'freshWater': 1, 'greyWater': 2, 'liveWell': 3, 'Oil': 4, 'wasteWater': 5 }
+SIGNALK_TANK_PATH_TO_SERVICE = {}
+
+class SystemBus(dbus.bus.BusConnection):
+		def __new__(cls):
+				return dbus.bus.BusConnection.__new__(cls, dbus.bus.BusConnection.TYPE_SYSTEM)
+
+class SessionBus(dbus.bus.BusConnection):
+		def __new__(cls):
+				return dbus.bus.BusConnection.__new__(cls, dbus.bus.BusConnection.TYPE_SESSION)
+
+def dbusconnection():
+	return SessionBus() if 'DBUS_SESSION_BUS_ADDRESS' in os.environ else SystemBus()
+
+# SignalkTank implements a DBUS service for a single tank. This rather
+# crummy implementation strategy is forced on us by the very broken
+# venus.os tank monitoring implementation. 
+
+class SignalkTank:
+	def __init__(self, servicename, paths, deviceinstance, productname='Signal K tank', connection='Signal K tank service'):
+		self._dbus = dbusconnection();
+		self._dbusservice = VeDbusService(servicename, self._dbus)
+		self._paths = paths
+
+		self._dbusservice.add_path('/Mgmt/ProcessName', __file__)
+		self._dbusservice.add_path('/Mgmt/ProcessVersion', 'Unkown version, and running on Python ' + platform.python_version())
+		self._dbusservice.add_path('/Mgmt/Connection', 'SignalK' + str(deviceinstance))
+
+		self._dbusservice.add_path('/DeviceInstance', deviceinstance)
+		self._dbusservice.add_path('/ProductId', 0)
+		self._dbusservice.add_path('/ProductName', 'SignalK tank interface')
+		self._dbusservice.add_path('/FirmwareVersion', 0)
+		self._dbusservice.add_path('/HardwareVersion', 0)
+		self._dbusservice.add_path('/Connected', 1)
+ 
+		for path, settings in self._paths.iteritems():
+			self._dbusservice.add_path(path, settings['initial'], writeable=True, onchangecallback=self._handlechangedvalue)
+
+	def _update(self, currentLevel):
+	  self._dbusservice['/Level'] = (currentLevel * 100)
+	  self._dbusservice['/Remaining'] = (self._dbusservice['/Capacity'] * currentLevel)
+
+	def _handlechangedvalue(self, path, value):
+		logging.debug("someone else updated %s to %s" % (path, value))
+		return True # accept the change
+
+def updateTanks():
+	for path in SIGNALK_TANK_PATH_TO_SERVICE:
+		http = httplib.HTTPConnection(SIGNALK_SERVER)																   
+		http.request('GET', path)													  
+		res = http.getresponse()																						
+		if res.status == 200:																									   
+			currentLevel = json.loads(res.read())	  
+			SIGNALK_TANK_PATH_TO_SERVICE[path]._update(currentLevel)
+			logging.debug('updateTanks: updating service for %s with %f' % (path, currentLevel))
+		http.close()
+	return(1)
+
+# This program creates a collection of dbus services each of which
+# represents tank data recovered from the configured Signal K
+# server.
+#
+# Each service has a name like com.victronenergy.tank.signalk_tank_n
+# where 'n' is the N2K instance number of the tank as reported by
+# Signal K. Each service provides dbus data items in a format which
+# can be processed and displayed by the CCGX gui.
+
+def main():
+	logging.basicConfig(level=logging.INFO)
+
+	from dbus.mainloop.glib import DBusGMainLoop
+	DBusGMainLoop(set_as_default=True)
+
+	# If no Signal K tanks are configured, then auto-configure by
+	# recover all available tank paths from the server.
+	if not SIGNALK_TANKS:
+		http = httplib.HTTPConnection(SIGNALK_SERVER)
+		http.request('GET', SIGNALK_SELF_PATH + '/tanks/')
+		res = http.getresponse()
+		if res.status == 200:
+			jsondata = json.loads(res.read())
+			for fluidtype in jsondata:
+				if fluidtype in N2K_FLUID_TYPES:
+					for instance in jsondata[fluidtype]:
+						fluidType = SIGNALK_TO_N2K_FLUID_TYPES[fluidtype]
+						capacity = jsondata[fluidtype][instance]['capacity']['value']
+						SIGNALK_TANK_PATHS.append({ 'path': 'tanks/' + fluidtype + '/' + instance })
+						SIGNALK_TANK_PATHS.sort(key=lambda x: x.get('path')[::-1])
+		http.close()
+		logging.info('%d tanks autoconfigured from the remote server' % len(SIGNALK_TANK_PATHS))
+
+	# For each configured tank path create a service with DBUS values
+	# that can be processed by the CCGX interface. 
+	if SIGNALK_TANKS:
+		for tank in SIGNALK_TANKS:
+			[ dummy, fluidtype, instance ] = tank['path'].split('/')
+			http = httplib.HTTPConnection(SIGNALK_SERVER)
+			http.request('GET', SIGNALK_SELF_PATH + '/' + tank['path'])
+			res = http.getresponse()
+			if res.status == 200:
+				jsondata = json.loads(res.read())
+				fluidType = SIGNALK_TO_N2K_FLUID_TYPES[fluidtype]
+				capacity = jsondata['capacity']['value']
+				serviceName = 'com.victronenergy.tank.signalk_tank_' + instance
+				service = SignalkTank(
+					servicename=serviceName,
+					deviceinstance=int(instance),
+					paths={
+						'/Level': { 'initial': 0 },
+						'/FluidType': { 'initial': fluidType },
+						'/Capacity': { 'initial': capacity },
+						'/Remaining': { 'initial': 0 }
+					}
+				)
+				SIGNALK_TANK_PATH_TO_SERVICE[SIGNALK_SELF_PATH + '/' + tank['path'] + '/currentLevel/value'] = service
+			http.close()
+
+		# And finally arrange to update tank date every so often.
+		gobject.timeout_add(UPDATE_INTERVAL, updateTanks)
+
+	mainloop = gobject.MainLoop()
+	mainloop.run()
+
+if __name__ == "__main__":
+	main()
+
